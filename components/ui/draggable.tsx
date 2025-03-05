3441885814fb7cd61e9735a9ed0aedf6eca88d7b
Dante Vettori <vettori.dante@itismeucci.com>
2025-03-05 21:40:07 +0100

    drag&drop perfect

--- components/ui/draggable.tsx
+++ components/ui/draggable.tsx
@@ -1,44 +1,72 @@
-"use client"
+"use client";
 
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
-const Draggable = ({children} : {children? : React.ReactNode}) => {
-    const [styles, setStyles] = useState({});
-    const [grab, setGrab] = useState(false);
-    const handleStartGrab = () => {
-        setGrab(true);
+const Draggable = () => {
+  const [position, setPosition] = useState({ x: 100, y: 100 });
+  const [isDragging, setIsDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setIsDragging(true);
+    setOffset({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isDragging) return;
+    setPosition({
+      x: e.clientX - offset.x,
+      y: e.clientY - offset.y,
+    });
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  useEffect(() => {
+    // Aggiungi i listener globali
+    if (isDragging) {
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+    } else {
+      // Rimuovi i listener quando non stai trascinando
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
     }
-    const handleEndGrab = (e : any) => {
-        setGrab(false);
-        setStyles( {left: (e.clientX - (e.target.clientWidth/2))+"px",
-            top: (e.clientY - (e.target.clientHeight/2))+"px"})
-    }
 
-    const handleMove = (e : any) => {
-        if(grab){
-            setStyles( {left: (e.clientX - (e.target.clientWidth/2))+"px",
-                top: (e.clientY - (e.target.clientHeight/2))+"px"})
-        }
-    }
+    // Cleanup dei listener quando il componente viene smontato
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [isDragging]); // Effettua il controllo quando `isDragging` cambia
 
-    return (
-        <div
-        >
-            <div 
-            draggable
-            style={styles}
-            onDragStart={handleStartGrab}
-            onDrag={handleMove}
-            onDragEnd={handleEndGrab}
-            className={`${grab ? "cursor-grabbing" : "hover:cursor-grab"} select-none absolute`}
-            >
-                yomo
-            </div>
-            <div>
-                {children}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        position: "absolute",
+        left: position.x,
+        top: position.y,
+        userSelect: "none",
+      }}
+    >
+      <div
+        onMouseDown={handleMouseDown}
+        className={`min-w-32 min-h-16 bg-red-600 ${
+          isDragging ? "cursor-grabbing" : "cursor-grab"
+        }`}
+      >
+        Drag me
+      </div>
+      <div>
+        fjsdfshbjlkgdfhlsj
+      </div>
+    </div>
+  );
+};
 
 export default Draggable;

8eb72bea295fd09b2bd7617fbaad4e4a5a94330b
Dante Vettori <vettori.dante@itismeucci.com>
2025-03-15 13:26:17 +0100

    working appSim with zindex

--- app/protected/campaign/[id]/DM.tsx
+++ app/protected/campaign/[id]/DM.tsx
@@ -1,6 +1,7 @@
 import ApplicationSim from "@/components/ui/applicationsim";
 import { fetchCampaignPending } from "@/lib/data-fetcher";
 import PendingHandler from "../pendingHandler";
+import DesktopSim from "../desktop";
 
 export const DM = async ({campID} : {campID : string}) => {
     const pending = await fetchCampaignPending(campID);    
@@ -16,12 +17,17 @@ export const DM = async ({campID} : {campID : string}) => {
                 }
             </div>
         }
-        <div className="grid grid-cols-2">
-            <ApplicationSim appName="Powa">
+        <DesktopSim className="grid grid-cols-6 gap-8 p-8">
+            <ApplicationSim appInfo={{appName: "11", status:"open",id: 1}}>
                 minne
             </ApplicationSim>
-
-        </div>
+            <ApplicationSim appInfo={{appName: "22", status:"open",id: 2}}>
+                minne
+            </ApplicationSim>
+            <ApplicationSim appInfo={{appName: "33", status:"open",id: 3}}>
+                minne
+            </ApplicationSim>
+        </DesktopSim>
     </div>
     )
 }
--- components/ui/applicationsim.tsx
+++ components/ui/applicationsim.tsx
@@ -5,51 +5,60 @@ import { Direction } from "re-resizable/lib/resizer";
 import { Minus, Square, X } from "lucide-react";
 import { Resizable } from "re-resizable";
 import { useEffect } from "react";
+import { useDesktop } from "@/app/protected/campaign/desktop";
+import { random, round } from "lodash";
 
 const ApplicationSim = (
-  {appName, children, className, set}
-   : 
-  {appName: string, children: React.ReactNode, className?: string, 
-    set? : {width?: string, height?: string, position?: {x: number, y:number}}
+  { appInfo, children, className, set }: 
+  { appInfo: {appName: string, status: "open" | "hidden" | "close", id: string | number},
+    children: React.ReactNode, className?: string, 
+    set?: { width?: string, height?: string, position?: { x: number, y: number } }
   }) => {
   const { activator, isDragging, draggableRef, style, setWidth, setHeight, position, setPosition } = useDraggable();
+  const desktop = useDesktop();
 
   useEffect(() => {
-
-    if(set !== undefined){
-      if(set.position !== undefined){
+    if (set !== undefined) {
+      if (set.position !== undefined) {
         setPosition(position);
       }
-      if(set.height !== undefined) setHeight(set.height);
-      if(set.width !== undefined) setWidth(set.width);
+      if (set.height !== undefined) setHeight(set.height);
+      if (set.width !== undefined) setWidth(set.width);
     }
-  }, [])
+  }, [set, setPosition, setHeight, setWidth, position]);
+
+  useEffect(() => {
+    if (desktop) {
+      if(desktop.addApp(appInfo.appName, appInfo.status, appInfo.id) === undefined){
+        let corr : number= random(100);
+        appInfo.id = corr;
+        desktop.addApp(appInfo.appName, appInfo.status, appInfo.id);
+      }
+    }
+  }, []);
 
   const handleResize = (e: MouseEvent | TouchEvent, dir: Direction, ref: HTMLElement) => {
     if (ref.style.width !== "auto") {
       setWidth(ref.style.width);
     }
-
     setHeight(ref.style.height);
     if (dir.toLowerCase().includes("left") && e instanceof MouseEvent) {
       setPosition({ x: e.screenX, y: position.y });
     }
     if (dir.toLowerCase().includes("top") && e instanceof MouseEvent) {
-      setPosition({ x: position.x, y: e.screenY })
+      setPosition({ x: position.x, y: e.screenY });
     }
-    if(dir == "topLeft" && e instanceof MouseEvent){
-      setPosition({ x: e.screenX, y: e.screenY })
-
+    if (dir === "topLeft" && e instanceof MouseEvent) {
+      setPosition({ x: e.screenX, y: e.screenY });
     }
-    
-  }
-
+  };
 
   return (
     <div
       ref={draggableRef}
-      style={style}
-      className="rounded border bg-black"
+      style={{...style, zIndex: desktop.getZindex(appInfo.id)}}
+      className={` ${className === undefined ? "rounded border bg-black" : className}`}
+      onMouseDown={() => {desktop.activeApp(appInfo.id)}}
     >
       <Resizable
         size={{ height: style.height }}
@@ -58,12 +67,11 @@ const ApplicationSim = (
       >
         <div
           ref={activator}
-          className={`p-2 rounded-t bg-gray-600 flex justify-between ${isDragging ? "cursor-grabbing" : "cursor-grab"
-            }`}
+          className={` p-2 rounded-t bg-gray-600 flex justify-between ${isDragging ? "cursor-grabbing" : "cursor-grab"}`}
         >
-          <span>Appname</span> <span className="flex"> <Minus /><Square /><X /></span>
+          <span>{appInfo.appName}</span> <span className="flex"> <Minus /><Square /><X /></span>
         </div>
-            {children}
+        {children}
       </Resizable>
     </div>
   );
